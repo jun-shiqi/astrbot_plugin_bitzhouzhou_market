55b9caf04687fedd8b9f73c84c51fbab7a410713
jun-shiqi <3068956090@qq.com>
2026-03-28 20:24:00 +0800

    4
    1

--- core/broadcaster.py
+++ core/broadcaster.py
@@ -1,10 +1,12 @@
 import logging
 from typing import Dict, List
 from astrbot.api.platform import Platform
+from astrbot.api.event import MessageChain
+
 logger = logging.getLogger(__name__)
 
 
-async def send_message(self, targets: Dict[str, List[str]], message: str):
+async def send_message(self,event:AstrMessageEvent ,targets: Dict[str, List[str]], message: str):
     """
     统一发送消息函数
 
@@ -23,11 +25,14 @@ async def send_message(self, targets: Dict[str, List[str]], message: str):
         private_users = targets.get("private_users", [])
         for user_id in private_users:
             try:
-                await adapter.send_msg(
-                    target_type="private",
-                    target_id=user_id,
-                    message=message
-                )
+                # await adapter.send_msg(
+                #     target_type="private",
+                #     target_id=user_id,
+                #     message=message
+                # )
+                umo = event.unified_msg_origin
+                message_chain = MessageChain().message(message)
+                await self.context.send_message(event.unified_msg_origin, message_chain)
                 logger.info(f"成功向用户 {user_id} 发送消息")
             except Exception as e:
                 logger.error(f"向用户 {user_id} 发送消息失败: {e}")
